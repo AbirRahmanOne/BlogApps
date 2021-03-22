6e3486800e5e371af00d6243e1b4c67d4e3209d1
AbirRahmanOne <abir.talks@gmail.com>
2021-03-23 01:19:23 +0600

    view engine

--- app.js
+++ app.js
@@ -11,13 +11,22 @@ const port = 3000 ;
 
 
 app.get('/', (req, res)=>{
-    res.render('index');
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req,res)=>{
-    res.render('about');
+    res.render('about', {title:'About'});
 })
 
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new blog' });
+  });
 
 // 404 pages
 app.use( (req, res)=>{
